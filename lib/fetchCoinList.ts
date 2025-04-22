f8b7e8f853e735c976364832d974c2abd4555218
Pobaji93 <binner.jonathan@hotmail.de>
2025-04-23 00:16:45 +0200

    fetchCoinList.ts aktualisieren

--- lib/fetchCoinList.ts
+++ lib/fetchCoinList.ts
@@ -1,16 +1,22 @@
 // lib/fetchCoinList.ts
 
-let coinListCache: any[] | null = null;
+let coinListCache: {
+  id: string;
+  symbol: string;
+  name: string;
+  image: string;
+}[] | null = null;
 
-export async function fetchCoinList(): Promise<
-  { id: string; symbol: string; name: string; image: string }[]
-> {
+export async function fetchCoinList(): Promise<{
+  id: string;
+  symbol: string;
+  name: string;
+  image: string;
+}[]> {
   if (coinListCache) return coinListCache;
 
   try {
-    const res = await fetch(
-      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
-    );
+    const res = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&per_page=100&page=1");
     const data = await res.json();
 
     coinListCache = data.map((coin: any) => ({
