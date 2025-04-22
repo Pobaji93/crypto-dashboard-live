5681ba10d0858c83f02c8f925061ca6ee71c6c5f
Pobaji93 <binner.jonathan@hotmail.de>
2025-04-22 22:20:25 +0200

    Create coinLogos.ts

---
+++ lib/coinLogos.ts
@@ -0,0 +1,5 @@
+export const coinLogos: Record<string, string> = {
+  BTC: "https://cryptologos.cc/logos/bitcoin-btc-logo.png?v=029",
+  ETH: "https://cryptologos.cc/logos/ethereum-eth-logo.png?v=029",
+  // Weitere Logos kannst du hier ergänzen
+};
