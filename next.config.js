e17d21b6abce2123b1ec5003b6a145cac479ef21
Jonathan Binner <jonathan@Jonathans-MacBook-Air.local>
2025-04-23 22:27:52 +0200

    🛠 Fix: Webpack-Konfiguration für Vercel-Build stabilisiert

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-module.exports = nextConfig;
+// next.config.js
+module.exports = {
+  webpack: (config) => {
+    config.externals.push({
+      'bufferutil': 'commonjs bufferutil',
+      'utf-8-validate': 'commonjs utf-8-validate',
+    });
+    return config;
+  },
+};
